3eea0b3be946998d6ff45e342edd8cc9b28cacd5
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-07-07 16:23:30 -0300

    v. 1.5.2

--- logica.js
+++ logica.js
@@ -1,39 +1,49 @@
 var numero = 0
 let checkbox = document.getElementById("checkbox");
 
-function mais() {
 
+// temeTime🌙🌞
+function tame() {
+  var data = new Date() 
+  var hora = data.getHours()
+
+  if (hora > 6 && hora < 18) {   
+    document.body.style.backgroundColor = "#C0DAEB";
+    document.getElementById("main").style.color = "#000000";
+    document.getElementById("contagem").style.color = "#000000";
+  } else if(hora > 18 && hora < 6) {
+    document.body.style.backgroundColor = "#152238";
+    document.getElementById("main").style.color = "#f5f5f5";
+    document.getElementById("contagem").style.color = "#f5f5f5";
+  }
+}
+
+
+// buttom🔢
+function mais() {
   document.getElementById('contagem').innerHTML = numero = numero + 1
   
 }
 
 function menos() {
-
   document.getElementById('contagem').innerHTML = numero = numero - 1
 
 }
 
 function reset() {
-
   document.getElementById('contagem').innerHTML = numero = 0
 
 }
 
+// return🏃
 document.getElementById('contagem').innerHTML = numero 
 
+// evento visual👀
 function evento() {
-  if(checkbox.checked) {
-    document.body.style.backgroundColor = "#152238";
-    document.getElementById("main").style.color = "#f5f5f5";
-    document.getElementById("contagem").style.color = "#f5f5f5";
 
-  } else {
-    document.body.style.backgroundColor = "#C0DAEB";
-    document.getElementById("main").style.color = "#000000";
+  if (numero == 0) {
     document.getElementById("contagem").style.color = "#000000";
-  }
-
-  if (numero < 0) {
+  } if (numero < 0) {
     document.getElementById("contagem").style.color = "#cf0000";
   } if (numero > 0) {
     document.getElementById("contagem").style.color = "#1ba4ff";
